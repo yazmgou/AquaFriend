437833b333cd7ed7063c2918bdcbfa60cb4b1293
vavidal <va.vidala@duocuc.cl>
2025-09-09 10:35:45 -0300

    Creacion de "home"

--- AquaFriends/AquaFriend/src/app/app.routes.ts
+++ AquaFriends/AquaFriend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home },  // Página principal
+  { path: '**', redirectTo: '' }           // Rutas inválidas → Home
+];
